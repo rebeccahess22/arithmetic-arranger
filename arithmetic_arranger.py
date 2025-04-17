1399fe3af3d593c0379221fcaa76eb04f9a5fd48
Rebecca Hess <84552271+rebeccahess22@users.noreply.github.com>
2025-04-17 19:15:07 -0400

    Update arithmetic_arranger.py

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -16,7 +16,7 @@ def arithmetic_arranger(problems, show_answers=False):
     if len(problems) > 5:
         return "Error: Too many problems."
 
-	#making empty lists that will store the formatted values
+    #making empty lists that will store the formatted values
     top_level = []
     bottom_level = []
     dash_line = []
@@ -34,20 +34,20 @@ def arithmetic_arranger(problems, show_answers=False):
         else:
             return "Error: Operator must be '+' or '-'."
 		
-		#removing leading or trailing zeros
+	#removing leading or trailing zeros
         first = parts[0].strip() 
         second = parts[1].strip()
 		
-		#both values must only contain digits
+	#both values must only contain digits
         if not first.isdigit() or not second.isdigit():
             return "Error: Numbers must only contain digits."
 		
-		#both values can only be a max length of 4 digits
+	#both values can only be a max length of 4 digits
         if len(first) > 4 or len(second) > 4:
             return "Error: Numbers cannot be more than four digits."
 		
-		#we will need to add leading spaces so that both valeus have the same length
-		# add 2 for an additional space and the operand in the second line
+	#we will need to add leading spaces so that both valeus have the same length
+	# add 2 for an additional space and the operand in the second line
         width = max(len(first), len(second)) + 2
         top_level.append(first.rjust(width))
         bottom_level.append(operator + second.rjust(width - 1))
@@ -67,7 +67,7 @@ def arithmetic_arranger(problems, show_answers=False):
         '    '.join(bottom_level) + '\n' +
         '    '.join(dash_line)
     )
-	#append the answers
+    #append the answers
     if show_answers:
         arranged_problems += '\n' + '    '.join(answers)
 
